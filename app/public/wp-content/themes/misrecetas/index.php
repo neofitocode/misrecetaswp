e726f0288f8af5c05e82120eb240c8f43cb82fd0
neofitocode <77021185+neofitocode@users.noreply.github.com>
2021-02-12 01:22:15 +0100

    Corrección de errores, menú wordpress

--- app/public/wp-content/themes/misrecetas/index.php
+++ app/public/wp-content/themes/misrecetas/index.php
@@ -7,25 +7,25 @@
                         <div class="col-desk-6 col-tab-6 mb-2">
                             <a href="receta.html">
                                 <h2 class="head-strong txt-center mb-05">Lubina con hummus</h2>                            
-                                <img class="img-md" src="<?php echo get_stylesheet_directory_uri(); ?>_resources/lubina-con-hummus-de-guisantes.jpg">
+                                <img class="img-md" src="<?php echo get_stylesheet_directory_uri(); ?>/_resources/lubina-con-hummus-de-guisantes.jpg">
                             </a>
                         </div>
                         <div class="col-desk-6 col-tab-6 mb-2">
                             <a href="receta.html">
                                 <h2 class="head-strong txt-center mb-05">Tiramisú express</h2>
-                                <img class="img-md" src="<?php echo get_stylesheet_directory_uri(); ?>_resources/tiramisu-express.jpg">
+                                <img class="img-md" src="<?php echo get_stylesheet_directory_uri(); ?>/_resources/tiramisu-express.jpg">
                             </a>
                         </div>
                         <div class="col-desk-6 col-tab-6 mb-2">
                             <a href="receta.html">
                                 <h2 class="head-strong txt-center mb-05">Espaguettis de calabacín</h2>
-                                <img class="img-md" src="<?php echo get_stylesheet_directory_uri(); ?>_resources/espaguetis-de-calabacin.jpg">
+                                <img class="img-md" src="<?php echo get_stylesheet_directory_uri(); ?>/_resources/espaguetis-de-calabacin.jpg">
                             </a>
                         </div>
                         <div class="col-desk-6 col-tab-6 mb-2">
                             <a href="receta.html">
                                 <h2 class="head-strong txt-center mb-05">Vichyssoise de espárragos</h2>
-                                <img class="img-md" src="<?php echo get_stylesheet_directory_uri(); ?>_resources/vichyssoise-de-esparragos-blancos.jpg">
+                                <img class="img-md" src="<?php echo get_stylesheet_directory_uri(); ?>/_resources/vichyssoise-de-esparragos-blancos.jpg">
                             </a>
                         </div>
                     </div>
@@ -37,15 +37,15 @@
                         <div class="grid">
                             <div class="col-mob-4 mb-1">
                                 <h3 class="fnt-md head-strong mb-05 txt-center">Entrante</h3>
-                                <a href="receta.html"><img class="img-md rounded" src="<?php echo get_stylesheet_directory_uri(); ?>_resources/vichyssoise-de-esparragos-blancos.jpg"></a>
+                                <a href="receta.html"><img class="img-md rounded" src="<?php echo get_stylesheet_directory_uri(); ?>/_resources/vichyssoise-de-esparragos-blancos.jpg"></a>
                             </div>
                             <div class="col-mob-4 mb-1">
                                 <h3 class="head-strong mb-05 txt-center">Principal</h3>
-                                <a href="receta.html"><img class="img-md rounded" src="<?php echo get_stylesheet_directory_uri(); ?>_resources/lubina-con-hummus-de-guisantes.jpg"></a>
+                                <a href="receta.html"><img class="img-md rounded" src="<?php echo get_stylesheet_directory_uri(); ?>/_resources/lubina-con-hummus-de-guisantes.jpg"></a>
                             </div>
                             <div class="col-mob-4 mb-1">
                                 <h3 class="head-strong mb-05 txt-center">Postre</h3>
-                                <a href="receta.html"><img class="img-md rounded" src="<?php echo get_stylesheet_directory_uri(); ?>_resources/tiramisu-express.jpg"></a>
+                                <a href="receta.html"><img class="img-md rounded" src="<?php echo get_stylesheet_directory_uri(); ?>/_resources/tiramisu-express.jpg"></a>
                             </div>
                         </div>                            
                     </div>    
